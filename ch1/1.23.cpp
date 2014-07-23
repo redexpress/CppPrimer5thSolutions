1c07d66e4123ccd9e3cd59067ad8587d8e10634d
Gavin Yanag <redppl@hotmaill.com>
2014-07-24 05:17:24 +0800

    update 1.23.cpp

--- ch1/1.23.cpp
+++ ch1/1.23.cpp
@@ -15,16 +15,17 @@ int main(){
 	map<string, size_t> isbnMap;
 	Sales_item item;
 	while(cin >> item){
-		map<string, size_t>::iterator iter;
-		string isbn = item.isbn();
-		iter = isbnMap.find(isbn);
-		if(iter != isbnMap.end()){
-			int count = iter->second;
-			isbnMap.erase(iter);
-			isbnMap.insert(pair<string, size_t>(isbn, ++count));
-		} else {
-			isbnMap.insert(pair<string, size_t>(isbn, 1));
-		}
+		isbnMap[item.isbn()]++;
+		// map<string, size_t>::iterator iter;
+		// string isbn = item.isbn();
+		// iter = isbnMap.find(isbn);
+		// if(iter != isbnMap.end()){
+		// 	int count = iter->second;
+		// 	isbnMap.erase(iter);
+		// 	isbnMap.insert(pair<string, size_t>(isbn, ++count));
+		// } else {
+		// 	isbnMap.insert(pair<string, size_t>(isbn, 1));
+		// }
 	}
 	for (auto pr: isbnMap){
 		cout << pr.first << " " << pr.second << endl;
