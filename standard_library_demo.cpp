7ba20e45fdb19755a2bc5ca69ce8e40d7132c4b3
Gavin Yanag <redppl@hotmaill.com>
2014-07-27 18:14:00 +0800

    add

---
+++ standard_library_demo.cpp
@@ -0,0 +1,232 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+//A.2.1
+void _find();
+void _find_if();
+void _count();
+void _count_if();
+void _all_of();
+void _any_of();
+void _none_of();
+
+void _adjacent_find();
+void _search_n();
+
+void _search();
+void _find_first_of();
+void _find_end();
+
+//A.2.2
+void _for_each();
+void _mismatch();
+void _equal();
+
+//A.2.3
+void _lower_bound();
+void _upper_bound();
+void _equal_range();
+void _binary_search();
+
+//A.2.4
+void _fill();
+void _fill_n();
+void _generate();
+void _generate_n();
+
+void _copy();
+void _copy_if();
+void _copy_n();
+void _move();
+void _transform();
+void _replace_copy();
+void _replace_copy_if();
+void _merge();
+
+void _iter_swap();
+void _swap_ranges();
+void _replace();
+void _replace_if();
+
+void _copy_backward();
+void _move_backward();
+void _inplace_merge();
+
+//A.2.5
+void _is_partitioned();
+void _partition_copy();
+void _partition_point();
+void _stable_partition();
+void _partition();
+
+void _sort();
+void _stable_sort();
+void _is_sorted();
+void _is_sorted_until();
+void _partial_sort();
+void _partial_sort_copy();
+void _nth_element();
+
+//A.2.6
+void _remove();
+void _remove_if();
+void _remove_copy();
+void _remove_copy_if();
+void _unique();
+void _unique_copy_if();
+void _rotate();
+void _rotate_copy();
+
+void _reverse();
+void _reverse_copy();
+void _random_shuffle();
+void _shuffle();
+
+//A.2.7
+void _is_permutation();
+void _next_permutation();
+void _prev_permutation();
+
+//A.2.8
+void _includes();
+void _set_union();
+void _set_intersection();
+void _set_difference();
+void _set_symmetric_difference();
+
+//A.2.9
+void _min();
+void _max();
+void _minmax();
+void _min_element();
+void _max_element();
+void _minmax_element();
+
+void _lexicographical_compare();
+
+//A.2.10
+void _accumulate();
+void _inner_product();
+void _partial_sum();
+void _adjacent_difference();
+void _iota();
+
+int main(){
+    return 0;
+}
+
+//A.2.1
+void _find(){}
+void _find_if(){}
+void _count(){}
+void _count_if(){}
+void _all_of(){}
+void _any_of(){}
+void _none_of(){}
+
+void _adjacent_find(){}
+void _search_n(){}
+
+void _search(){}
+void _find_first_of(){}
+void _find_end(){}
+
+//A.2.2
+void _for_each(){}
+void _mismatch(){}
+void _equal(){}
+
+//A.2.3
+void _lower_bound(){}
+void _upper_bound(){}
+void _equal_range(){}
+void _binary_search(){}
+
+//A.2.4
+void _fill(){}
+void _fill_n(){}
+void _generate(){}
+void _generate_n(){}
+
+void _copy(){}
+void _copy_if(){}
+void _copy_n(){}
+void _move(){}
+void _transform(){}
+void _replace_copy(){}
+void _replace_copy_if(){}
+void _merge(){}
+
+void _iter_swap(){}
+void _swap_ranges(){}
+void _replace(){}
+void _replace_if(){}
+
+void _copy_backward(){}
+void _move_backward(){}
+void _inplace_merge(){}
+
+//A.2.5
+void _is_partitioned(){}
+void _partition_copy(){}
+void _partition_point(){}
+void _stable_partition(){}
+void _partition(){}
+
+void _sort(){}
+void _stable_sort(){}
+void _is_sorted(){}
+void _is_sorted_until(){}
+void _partial_sort(){}
+void _partial_sort_copy(){}
+void _nth_element(){}
+
+//A.2.6
+void _remove(){}
+void _remove_if(){}
+void _remove_copy(){}
+void _remove_copy_if(){}
+void _unique(){}
+void _unique_copy_if(){}
+void _rotate(){}
+void _rotate_copy(){}
+
+void _reverse(){}
+void _reverse_copy(){}
+void _random_shuffle(){}
+void _shuffle(){}
+
+//A.2.7
+void _is_permutation(){}
+void _next_permutation(){}
+void _prev_permutation(){}
+
+//A.2.8
+void _includes(){}
+void _set_union(){}
+void _set_intersection(){}
+void _set_difference(){}
+void _set_symmetric_difference(){}
+
+//A.2.9
+void _min(){}
+void _max(){}
+void _minmax(){}
+void _min_element(){}
+void _max_element(){}
+void _minmax_element(){}
+
+void _lexicographical_compare(){}
+
+//A.2.10
+void _accumulate(){}
+void _inner_product(){}
+void _partial_sum(){}
+void _adjacent_difference(){}
+void _iota(){}
